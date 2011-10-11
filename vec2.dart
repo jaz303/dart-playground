3054e3e85f3ad2787114a60191fc6eb72b3acba7
Jason Frame <jason@onehackoranother.com>
2011-10-11 01:30:47 +0100

    negation operator, redirecting constructor

--- vec2.dart
+++ vec2.dart
@@ -3,12 +3,14 @@ class Vec2
 	final num EPSILON = 0.00001;
 
 	Vec2(num this.x, num this.y);
-	Vec2.zero() { x = 0.0; y = 0.0; }
+	Vec2.zero() : this(0, 0);
 	
 	Vec2 operator+(Vec2 rhs) => new Vec2(x + rhs.x, y + rhs.y);
 	Vec2 operator-(Vec2 rhs) => new Vec2(x - rhs.x, y - rhs.y);
 	Vec2 operator*(num rhs) => new Vec2(x * rhs, y * rhs);
 	Vec2 operator/(num rhs) => new Vec2(x / rhs, y / rhs);
+	
+	Vec2 operator negate() => new Vec(-x, -y);
 	
 	bool operator==(Vec2 rhs) =>
 		Math.abs(rhs.x - x) < EPSILON && Math.abs(rhs.y - y) < EPSILON;
