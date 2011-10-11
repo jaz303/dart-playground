fdc0eac104bc71b31bb87e5f8cd68aebd62954f5
Jason Frame <jason@onehackoranother.com>
2011-10-11 01:35:20 +0100

    some annotation

--- vec2.dart
+++ vec2.dart
@@ -1,17 +1,30 @@
 class Vec2
 {
+	// final denotes constant
 	final num EPSILON = 0.00001;
 
+	// default constructor - note the lack of a function body and the use of "this";
+	// causes parameters to be assigned directly to object fields
 	Vec2(num this.x, num this.y);
+	
+	// this is a named constructor, it is called by "new Vec2.zero()"
+	// this is also a redirecting constructor - it simply becomes new Vec2(0, 0)
+	// we could also have done: "Vec2.zero() { x = 0; y = 0; }"
 	Vec2.zero() : this(0, 0);
+	
+	// operator overloading for standard vector ops
 	
 	Vec2 operator+(Vec2 rhs) => new Vec2(x + rhs.x, y + rhs.y);
 	Vec2 operator-(Vec2 rhs) => new Vec2(x - rhs.x, y - rhs.y);
 	Vec2 operator*(num rhs) => new Vec2(x * rhs, y * rhs);
 	Vec2 operator/(num rhs) => new Vec2(x / rhs, y / rhs);
 	
+	// unary minus can be overloaded too
 	Vec2 operator negate() => new Vec(-x, -y);
 	
+	// equality operator
+	// this is invoked when comparing using "=="
+	// (Dart reserves === solely for comparing object identity and accordingly cannot be overloaded)
 	bool operator==(Vec2 rhs) =>
 		Math.abs(rhs.x - x) < EPSILON && Math.abs(rhs.y - y) < EPSILON;
 	
