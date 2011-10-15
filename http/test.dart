a31f14fa009efbc1dd8da68a7adae69d31de34a8
Jason Frame <jason@onehackoranother.com>
2011-10-15 14:36:40 +0100

    Test for pattern handler

--- http/test.dart
+++ http/test.dart
@@ -4,11 +4,25 @@ main() {
 	var server = new http.Server();
 	server.port = 1234;
 	
-	server.requestHandler = (http.Request request, http.Response response) {
-		response.contentType = 'text/plain';
-		response.body = request.requestURI;
-		response.end();
-	};
+	http.PatternHandler ph = new http.PatternHandler();
+	
+	ph.handlePattern("/foo",
+	    (http.Request _r, Match _m, http.Response r) => r.text("foo foo foo").end()
+	);
+	
+	ph.handlePattern("/bar",
+	    (http.Request _r, Match _m, http.Response r) => r.text("bar bar bar").end()
+	);
+	
+	ph.handlePattern(const RegExp(@"^/echo/(\w+)$"),
+	    (http.Request _r, Match m, http.Response r) => r.text("You said: ${m[1]}").end()
+	);
+	
+	ph.handlePattern(const RegExp(@"^.*$"),
+	    (http.Request _, Match m, http.Response r) => r.textStatus(404).end()
+	);
+	
+	server.requestHandler = ph;
 	
 	print("Starting server ${server}");
 	server.start();
